0bad89c59b8d9482a1da526de30740fc990282b2
Michail <rotari733@gmail.com>
2021-05-10 18:32:45 +0300

    Update class!!

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Privet");
+        System.out.println("Privet!");
+        System.out.println("yes!!");
     }
 }
